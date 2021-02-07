2fd048fc7ff2877f21920b65170c1cb9dcb9d07c
suihan <suihan742@gmail.com>
2021-02-07 16:10:47 +0900

    modify `EntriesType`

--- src/main/kotlin/com/suihan74/hatena/entry/EntriesType.kt
+++ src/main/kotlin/com/suihan74/hatena/entry/EntriesType.kt
@@ -1,11 +1,12 @@
 package com.suihan74.hatena.entry
 
 enum class EntriesType(val code : String) {
-    Hot("hotentry"),
-    Recent("newentry"),
+    HOT("hotentry"),
+
+    RECENT("newentry"),
     ;
 
     companion object {
-        fun fromOrdinal(idx: Int) = values().getOrElse(idx) { Hot }
+        fun fromOrdinal(idx: Int) = values().getOrElse(idx) { HOT }
     }
 }
